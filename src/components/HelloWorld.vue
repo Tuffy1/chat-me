8b1bf236e24880f618b04ecde684fe9741d8979f
joyee <704529989@qq.com>
2018-01-15 22:21:06 +0800

    commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login">
+    <input v-model="username" placeholder="Enter something..." style="width: 300px"><br>
+    <input v-model="password" placeholder="Enter something..." style="width: 300px"><br>
+    <Button type="ghost" @click="login()">Submit</Button>
+    <Button type="ghost">Cancel</Button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    ...mapActions(['login']),
+    login () {
+      this.$store.dispatch('login', {
+        username: this.username,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.login input {
+  margin: 10px 0;
+}
+</style>
---
+++ src/store/action.js
@@ -0,0 +1,4 @@
+export const login = ({content}, form) => {
+  console.log(form.username)
+  console.log(form.password)
+}
